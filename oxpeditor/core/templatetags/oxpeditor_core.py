07a35290801b59181c2716828e6801c150168e9a
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2010-12-29 01:52:17 +0000

    Improved choice of display of preferred/sort names in OxPoints editor.
    git-svn-id: https://svn.oucs.ox.ac.uk/groups/infodev/projects/oxpeditor@472 51206483-761b-46e0-9bf0-6548913a91db

--- oxpeditor/core/templatetags/oxpeditor_core.py
+++ oxpeditor/core/templatetags/oxpeditor_core.py
@@ -5,3 +5,11 @@ register = template.Library()
 @register.filter(name='any')
 def any_(value):
     return bool(value) and any(value)
+
+@register.filter
+def contains_comma(value):
+    return ', ' in value
+
+@register.filter(name='contains')
+def contains_(value, arg):
+    return arg in value
