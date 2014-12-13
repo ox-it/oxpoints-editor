f6dbb34d8dc549877066c19c77b9e559acaa28ff
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-12-13 22:26:09 +0000

    debian packaging magic

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup, find_packages
 
+# Idea borrowed from http://cburgmer.posterous.com/pip-requirementstxt-and-setuppy
+install_requires, dependency_links = [], []
+for line in open('requirements.txt'):
+    line = line.strip()
+    if line.startswith('-e'):
+        dependency_links.append(line[2:].strip())
+    elif line:
+        install_requires.append(line)
+
 setup(name='oxpeditor',
       version='0.1',
       packages=find_packages(),
       include_package_data=True,
-      package_data={'': ['*.xml', 'templates/*.html']})
+      package_data={'': ['*.xml', 'templates/*.html']},
+      install_requires=install_requires,
+      dependency_links=dependency_links)
