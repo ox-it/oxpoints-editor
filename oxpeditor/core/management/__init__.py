77b8d00ac1a67bf37052fb83a53417a27787810d
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-12-19 13:56:12 +0000

    Only run post_migrate hooks for our app

--- oxpeditor/core/management/__init__.py
+++ oxpeditor/core/management/__init__.py
@@ -1,23 +1,27 @@
-from django.db.models.signals import post_syncdb
+from django.db.models.signals import post_migrate
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 
 def create_conflict_user(sender, **kwargs):
+    if sender.name != 'oxpeditor.core':
+        return
     User = get_user_model()
     if not User.objects.filter(username='conflict').exists():
         User.objects.create_user('conflict')
 
-# Get the contenttypes post_syncdb hook called before ours.
+# Get the contenttypes post_migrate hook called before ours.
 import django.contrib.contenttypes.management
 
 def allow_itss_editing(sender, **kwargs):
+    if sender.name != 'oxpeditor.core':
+        return
     itss_group, _ = Group.objects.get_or_create(name='itss')
 
     content_type = ContentType.objects.get(app_label='core', model='object')
     permission = Permission.objects.get(content_type=content_type, codename='change_object')
     itss_group.permissions.add(permission)
 
-post_syncdb.connect(create_conflict_user)
-post_syncdb.connect(allow_itss_editing)
+post_migrate.connect(create_conflict_user)
+post_migrate.connect(allow_itss_editing)
 
