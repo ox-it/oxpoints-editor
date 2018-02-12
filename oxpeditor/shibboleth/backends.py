c7276083f2815d79680935abd5d56bb094b9411e
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2018-02-12 14:19:55 +0000

    Fix group setup in auth process

--- oxpeditor/shibboleth/backends.py
+++ oxpeditor/shibboleth/backends.py
@@ -31,21 +31,21 @@ class ShibbolethBackend(object):
             'status:{}'.format(request_meta['oakStatus']),
         }
 
-        for orgunit_dn in request_meta['orgunit-dn'].split(';'):
+        for orgunit_dn in request_meta.get('orgunit-dn', '').split(';'):
             match = re.match('oakUnitCode=(.*),ou=units,dc=oak,dc=ox,dc=ac,dc=uk', orgunit_dn)
             if match:
                 groups.add('affilition:{}'.format(match.group(1)))
 
-        for oak_itss_for in request_meta['oakITSSFor'].split(';'):
+        for oak_itss_for in request_meta.get('oakITSSFor', '').split(';'):
             match = re.match('oakGN=ITSS,oakUnitCode=(.*),ou=units,dc=oak,dc=ox,dc=ac,dc=uk', oak_itss_for)
             if match:
                 groups.add('itss')
                 groups.add('itss:{}'.format(match.group(1)))
 
-        user.groups.remove(user.groups.exclude(name__in=groups).filter(Q(name='itss') | Q(name__startswith='itss:') |
+        user.groups.remove(*user.groups.exclude(name__in=groups).filter(Q(name='itss') | Q(name__startswith='itss:') |
                                                                        Q(name__startswith='affiliation:') |
                                                                        Q(name__startswith='status:')))
-        user.groups.add(groups)
+        user.groups.add(*[Group.objects.get_or_create(g)[0] for g in groups])
 
         return user
 
